8f4b7b73bcf48562a49964e9a59674932b9c4a74
harman-ransi-1016 <ransih24@students.rowan.edu>
2022-10-20 13:10:04 -0400

    added models

--- music/models.py
+++ music/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    song_id = models.CharField(max_length=100)
+    song_name = models.CharField(max_length=50)
