9db6b719d3097e4379c75c8a6b1ff646dba2d65b
Alex Oeser <alexoeser2@gmail.com>
2022-11-08 13:24:55 -0500

    fixed issue with register redirect

--- users/views.py
+++ users/views.py
@@ -68,6 +68,6 @@ class RegisterView(View):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
 
-            return redirect(to='login')
+            return redirect(to='../login')
 
         return render(request, self.template_name, {'form': form})
