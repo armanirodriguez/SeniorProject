4a6a357891b786088d6ab25ecfa9b4d63509d71a
Andrew Hehl <andrewhehls@gmail.com>
2022-11-09 20:49:13 -0500

    display bug fixes

--- static/groups/js/scrollbar.js
+++ static/groups/js/scrollbar.js
@@ -1,8 +1,12 @@
-const songContainer = document.getElementById('song_scroll');
+const songContainer = document.getElementById("song_scroll");
 const songContainerWidth = songContainer.scrollWidth;
-
 window.addEventListener('load', () => {
     self.setInterval(() => {
-      songContainer.scrollLeft += 50;
-    }, 15);
+      if(songContainer.scrollLeft <= songContainerWidth){
+        songContainer.scrollLeft = songContainer.scrollLeft + 50;
+      }
+      else{
+        songContainer.scrollLeft -= 50;
+      }
+    }, 50);
   });
