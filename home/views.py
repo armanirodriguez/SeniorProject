814a9bcc64475d86f4683c3e59a2199bc5859dc1
Alex Oeser <alexoeser2@gmail.com>
2022-11-17 13:33:45 -0500

    stats on home page now present

--- home/views.py
+++ home/views.py
@@ -2,9 +2,14 @@ from django.http import Http404
 from django.shortcuts import render
 
 from groups.models import Post
+from groups.models import Community
+
+
 # Create your views here.
 def home(request):
     posts = Post.objects.all().order_by('-timestamp')
+    communities = Community.objects.all();
     return render(request, 'home.html', {
-        'posts' : posts
+        'posts': posts,
+        'communities': communities,
     })
