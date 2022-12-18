aefc9fd5de62193aaba0954296a5a1efa0162418
Joseph DiTaranto <56172275+josephad2001@users.noreply.github.com>
2022-12-17 19:46:19 -0500

    Sort By Upvotes

--- groups/urls.py
+++ groups/urls.py
@@ -6,7 +6,9 @@ app_name = "groups"
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('', views.home, name='home'),
+    path('v', views.homeVote, name='homeVote'),
     path('c/<str:community_name>/', views.community, name='community'),
+    path('cs/<str:community_name>/', views.communityVote, name='communityVote'),
     path("joincommunity/<str:community_name>", views.join_community, name='join_community'),
     path("leavecommunity/<str:community_name>", views.leave_community, name='leave_community'),
     path("rate/<int:post_id>/<int:rating>", views.rate, name='rate'),
--- groups/views.py
+++ groups/views.py
@@ -67,6 +67,64 @@ def home(request):
         'comment_form': CommentForm(),
         'createCommunityForm': communityForm
     })
+    
+def homeVote(request):
+    communityForm = createCommunityForm(request.POST)
+    try:
+        if(request.user.is_authenticated):
+            user_profile = request.user.profile
+        else:
+            user_profile = ""
+    except Profile.DoesNotExist:
+        user_profile = Profile(user=request.user)
+    users = Profile.objects.all()
+    songs = None
+    if request.method == "POST":
+        if 'post_flag' in request.POST:
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.poster = request.user
+                post.save()
+                add_songs = list(filter(None, request.POST['songs_input'].split(',')))
+                sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='616f55f94a904037a1eb6bdfbfa96d95', client_secret='2e262cfa37904ebeb74d43ff6903c9de'))
+                if(add_songs):
+                    for song in add_songs:
+                        try:
+                            newsong = Song(song_id = song)
+                            song_name = sp.track(song).get('name')
+                            newsong.song_name = song_name
+                            newsong.save()
+                        except:
+                            print("song already in library")
+                        tempsong = get_object_or_404(Song, song_id = song)
+                        post.songs.add(tempsong)
+                post.save()
+                return HttpResponseRedirect(request.path_info)
+        if 'search_flag' in request.POST:
+            searchForm = SearchForm(request.POST)
+            if searchForm.is_valid():
+                query = searchForm.cleaned_data['query']
+                songs = search_songs(query)
+    posts = Post.objects.all().order_by('-timestamp')
+    try:
+        test = user_profile.joined_communities.all()
+        posts = Post.objects.all().filter(community__in=test).order_by('-score')
+    except:
+        posts = None
+    
+    # TODO: add community form to this
+    return render(request, 'communityhome.html', {
+        'form' : PostForm(),
+        'posts' : posts,
+        'communities' : Community.objects.all(),
+        'searchForm': SearchForm(),
+        'songs': songs,
+        'user_profile': user_profile,
+        'users': users,
+        'comment_form': CommentForm(),
+        'createCommunityForm': communityForm
+    })
 
 def search_songs(query):
     sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='616f55f94a904037a1eb6bdfbfa96d95', client_secret='2e262cfa37904ebeb74d43ff6903c9de'))
@@ -115,6 +173,60 @@ def community(request, community_name):
                     query = searchForm.cleaned_data['query']
                     songs = search_songs(query)
     posts = Post.objects.all().filter(community=community).order_by('-timestamp')
+    return render(request, 'communityhome.html', {
+        'community' : community,
+        'form' : PostForm(initial={'community' : community}),
+        'posts' : posts,
+        'songs' : songs,
+        'searchForm': SearchForm(),
+        'communities' : Community.objects.all(),
+        'comm_users': comm_users,
+        'user_profile': user_profile,
+        'comment_form': CommentForm(),
+        'createCommunityForm': communityForm
+    })
+    
+def communityVote(request, community_name):
+    communityForm = createCommunityForm(request.POST)
+    songs = ""
+    try:
+        if(request.user.is_authenticated):
+            user_profile = request.user.profile
+        else:
+            user_profile = ""
+        community = Community.objects.get(name=community_name)
+        comm_users = Profile.objects.filter(joined_communities__in=[community.id]).values()
+    except Community.DoesNotExist:
+        raise Http404('Community not found')
+    if request.method == "POST":
+        if 'post_flag' in request.POST:
+                form = PostForm(request.POST)
+                if form.is_valid():
+                    post = form.save(commit=False)
+                    post.poster = request.user
+                    add_songs = list(filter(None, request.POST['songs_input'].split(',')))
+                    post.save()
+                    print(add_songs)
+                    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='616f55f94a904037a1eb6bdfbfa96d95', client_secret='2e262cfa37904ebeb74d43ff6903c9de'))
+                    if(add_songs):
+                        for song in add_songs:
+                            try:
+                                newsong = Song(song_id = song)
+                                song_name = sp.track(song).get('name')
+                                newsong.song_name = song_name
+                                newsong.save()
+                            except:
+                                print("song already in library")
+                            tempsong = get_object_or_404(Song, song_id = song)
+                            post.songs.add(tempsong)
+                    post.save()
+                    return HttpResponseRedirect(request.path_info)
+        if 'search_flag' in request.POST:
+                searchForm = SearchForm(request.POST)
+                if searchForm.is_valid():
+                    query = searchForm.cleaned_data['query']
+                    songs = search_songs(query)
+    posts = Post.objects.all().filter(community=community).order_by('-score')
     return render(request, 'communityhome.html', {
         'community' : community,
         'form' : PostForm(initial={'community' : community}),
